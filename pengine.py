4f225dc8863e2d8b543c16567979303bd9324c61
owen1050 <owen1050@gmail.com>
2020-02-03 11:41:31 -0500

    updated with better rliabillity and speed

--- pengine.py
+++ pengine.py
@@ -5,8 +5,9 @@ import threading
 def doTrick():
 	global lock
 	while True:
+		time.sleep(0.01)
 		if lock:
-			time.sleep(0.01)
+			pass
 		else:
 			pyautogui.keyDown('up')
 
@@ -17,39 +18,56 @@ i = 0
 delayTime = .8
 holdTime = 1.4
 lastTime = time.time()
+crashed = True
 while True:
-	if(time.time() -20 > lastTime):
-		pyautogui.click(x=984, y=832) 
-		time.sleep(1)
-		pyautogui.click(x=1175, y=383) 
+	if(time.time() - 10> lastTime and crashed):
+		pyautogui.keyDown('space')
+		time.sleep(0.05)
+		pyautogui.keyDown('space')
+		time.sleep(0.5)
+		crashed = False
+		print("presssed space to try and recover")
+		if(pyautogui.pixelMatchesColor(1173, 337, (2, 128, 205))):
+			lastTime = 0
+	if(time.time() - 20> lastTime):
+		print("end of round, initiated reset")
+		pyautogui.click(x=1257, y=216) 
+		time.sleep(0.05)
+		pyautogui.click(x=1257, y=216) 
+		while(False == pyautogui.pixelMatchesColor(1368, 738, (120, 149, 187))):
+			time.sleep(0.1)
+		time.sleep(0.5)
+		pyautogui.click(x=1500, y=315) 
 		time.sleep(0.01)
-		pyautogui.click(x=1175, y=383) 
-		time.sleep(3)
-		pyautogui.click(x=1493, y=443) 
+		pyautogui.click(x=1500, y=315) 
+		while(False == pyautogui.pixelMatchesColor(677, 570, (2, 128, 205))):
+			time.sleep(0.1)
+		time.sleep(0.5)
+		pyautogui.click(x=860, y=512) 
 		time.sleep(0.01)
-		pyautogui.click(x=1493, y=443) 
-		time.sleep(4)
-		pyautogui.click(x=831, y=550) 
-		time.sleep(3)
+		pyautogui.click(x=860, y=512) 
+		while(False == pyautogui.pixelMatchesColor(764, 996, (87, 30, 0))):
+			time.sleep(0.1)
+		time.sleep(0.5)
 		pyautogui.click(x=1439, y=847) 
 		time.sleep(0.01)
-		pyautogui.click(x=1439, y=847) 
-		time.sleep(3)
+		pyautogui.click(x=1439, y=847)
 		lastTime = time.time()
 	arrowLowcation = pyautogui.locateOnScreen("yellow.png", region= (450,500,1000,1))
 	if arrowLowcation is not None:
 		lock = True
+		crashed = True
+		print(time.time() -lastTime)
 		lastTime = time.time()
-		print("saw somthing")
 		if arrowLowcation[0] < 960:
-			print("left")
+			print("saw somthing left, turning right")
 			time.sleep(delayTime)
 			start = time.time()
 			while time.time() - holdTime < start:
 				pyautogui.keyDown('right')
 			pyautogui.keyUp('right')
 		else:
-			print("right")
+			print("saw somthing right, turning left")
 			time.sleep(delayTime)
 			start = time.time()
 			while time.time() - holdTime < start:
