1014d711600b274bbca9f800cdc8c161d62bf343
owen1050 <owen1050@gmail.com>
2020-02-05 01:14:38 -0500

    added version for cpreqritten

--- rewritter.py
+++ rewritter.py
@@ -17,7 +17,7 @@ t.start()
 i = 0
 counter = 0
 delayTime = .8
-holdTime = 1.4
+holdTime = 1.1
 lastTime = time.time()
 crashed = True
 while True:
@@ -28,32 +28,32 @@ while True:
 		time.sleep(0.5)
 		crashed = False
 		print("presssed space to try and recover")
-		if(pyautogui.pixelMatchesColor(1173, 337, (2, 128, 205))):
+		if(pyautogui.pixelMatchesColor(1173, 450, (2, 128, 205))):
 			lastTime = 0
 	if(time.time() - 20> lastTime):
 		counter = counter + 1
 		print("end of round, initiated reset")
-		pyautogui.click(x=1257, y=216) 
+		pyautogui.click(x=1200, y=370) 
 		time.sleep(0.05)
-		pyautogui.click(x=1257, y=216) 
-		while(False == pyautogui.pixelMatchesColor(1368, 738, (120, 149, 187))):
+		pyautogui.click(x=1200, y=370) 
+		while(False == pyautogui.pixelMatchesColor(1271, 848, (120, 149, 187))):
 			time.sleep(0.1)
 		time.sleep(0.5)
-		pyautogui.click(x=1500, y=315) 
+		pyautogui.click(x=1400, y=450) 
 		time.sleep(0.01)
-		pyautogui.click(x=1500, y=315) 
-		while(False == pyautogui.pixelMatchesColor(677, 570, (2, 128, 205))):
+		pyautogui.click(x=1400, y=450) 
+		while(False == pyautogui.pixelMatchesColor(750, 525, (2, 128, 205))):
 			time.sleep(0.1)
 		time.sleep(0.5)
-		pyautogui.click(x=860, y=512) 
+		pyautogui.click(x=860, y=622) 
 		time.sleep(0.01)
-		pyautogui.click(x=860, y=512) 
-		while(False == pyautogui.pixelMatchesColor(764, 996, (87, 30, 0))):
+		pyautogui.click(x=860, y=622) 
+		while(False == pyautogui.pixelMatchesColor(809, 1021, (87, 30, 0))):
 			time.sleep(0.1)
 		time.sleep(0.5)
-		pyautogui.click(x=1439, y=847) 
+		pyautogui.click(x=1289, y=617) 
 		time.sleep(0.01)
-		pyautogui.click(x=1439, y=847)  
+		pyautogui.click(x=1289, y=617)  
 		lastTime = time.time()
 		print("Begining round " + str(counter))
 	arrowLowcation = pyautogui.locateOnScreen("yellow.png", region= (450,500,1000,1))
